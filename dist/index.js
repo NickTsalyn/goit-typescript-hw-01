fdd2d2073a36aaa661d0e41225b5ce9c5254d857
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2023-11-03 17:17:42 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
+const name = "Maria";
+const age = 32;
+const user = {
+    apple: "green",
+    banana: 12
+};
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const name: string = "Maria"
+const age: number = 32
+
+
+type User = {
+    apple: string,
+    banana: number
+}
+
+const user: User = {
+    apple: "green",
+    banana: 12
+}
